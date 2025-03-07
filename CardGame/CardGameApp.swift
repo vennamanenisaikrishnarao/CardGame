5a2cd49f163d53b027831fff1708c15fe5326ec2
vennamanenisaikrishnarao <sai_krishna.vennamaneni@okstate.edu>
2025-03-06 20:31:23 -0600

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Vennamaneni, Sai Krishna on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
